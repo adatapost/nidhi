f8a2e23a68105a1aee08872fecaab5c5b496519a
adatapost <adatapost@gmail.com>
2015-10-16 08:59:15 +0530

    Added code

--- IOApp/src/ioapp/IOApp.java
+++ IOApp/src/ioapp/IOApp.java
@@ -1,9 +1,32 @@
 package ioapp;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class IOApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello! World");
+        try {
+            URL url = new URL("https://www.xyz.com/sample.zip");
+            URLConnection cn = url.openConnection();
+            try (BufferedInputStream in
+                    = new BufferedInputStream(cn.getInputStream())) {
+                try (BufferedOutputStream out
+                        = new BufferedOutputStream(
+                                new FileOutputStream("d:/javaprg/sample1.zip"))) {
+                            byte[] buffer = new byte[8192]; //8 KB
+                            int reads = 0;
+
+                            while ((reads = in.read(buffer)) > 0) {
+                                out.write(buffer, 0, reads);
+                            }
+                        }//end-try
+            }//end-try
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
-    
 }
